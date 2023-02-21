34a0d6bcd5220ca38713a5133688f35bcb81fadb
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-02-21 21:04:25 +0530

    Time: 18 ms (38.68%), Space: 15.7 MB (7.45%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int binarySearchedindex(vector<int>& arr, int startingIndex,int target){
+    int start=startingIndex;
+        int end = arr.size()-1;
+        int mid = start+(end-start)/2;
+        
+        while(start<=end){
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+            mid = start+(end-start)/2;
+        }
+        return -1;
+    }
+
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        for(int i=0;i<numbers.size();i++){
+            int newTarget = target-numbers[i];
+            int startingIndex = i+1;
+            int foundIndex = binarySearchedindex(numbers,startingIndex,newTarget);
+            
+            if(foundIndex != -1){
+                return {startingIndex,foundIndex+1};
+            }
+        }
+        return {};
+    }
+};
