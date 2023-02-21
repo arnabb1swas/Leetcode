d4602e1192b0619f07eea2e151d2958f516aa6bb
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-02-21 23:30:46 +0530

    Update 0121-best-time-to-buy-and-sell-stock.cpp

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,5 +1,16 @@
 class Solution {
   public: int maxProfit(vector < int > & prices) {
+    
+      // => [7,1,5,3,6,4]
+      // price   lssf    pist    op
+      //----------------------------
+      // 7       7       0       0
+      // 1       1       0       0
+      // 5       1       4       4
+      // 3       1       2       4
+      // 6       1       5       5
+      // 4       1       3       5
+    
       int lssf = INT_MAX; //least stock so far
       int op = 0; //overall profit
       int pist = 0; //profit if sold today
