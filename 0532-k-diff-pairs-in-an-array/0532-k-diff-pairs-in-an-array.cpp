45e8b2b066812e122dd16060745942d870b22e16
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-03-02 22:47:49 +0530

    Time: 24 ms (47.01%), Space: 13.2 MB (49.94%) - LeetHub

---
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        
+    // sort the array so that we can use 2 pointers method
+    sort(nums.begin(), nums.end());
+    int i = 0, j = 1;
+    set<pair<int, int>> pairCount;
+        
+    while (j < nums.size())
+    {
+        int diff = nums[j] - nums[i];
+        if (diff == k){
+            pairCount.insert({nums[i], nums[j]});
+            i++, j++;
+        }
+        else if (diff > k){ // if diff is getting bigger reduce it
+            i++;
+        }
+        else{ // if diff is getting smaller increase it
+            j++;
+        }
+// edge case 0
+// any number will be a pair, u can '-' the number from itself and you have a pair
+        if (i == j){ 
+            j++;
+        }
+    }
+
+    return pairCount.size();
+    }
+};
