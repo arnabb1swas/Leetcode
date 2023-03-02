047d9a1478ccaf849bf065ac677169f423ee006b
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-03-03 01:43:40 +0530

    Time: 15 ms (87.76%), Space: 13.1 MB (71.27%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,32 +1,34 @@
 class Solution {
   public:
-
-    int binarySearch(vector < int > arr, int start, int target) {
-      int end = arr.size() - 1;
-        
-      while (start <= end) {
-        int mid = start + (end - start) / 2;
-        if (arr[mid] == target)
-          return mid;
-        else if (arr[mid] > target)
-          end = mid - 1;
-        else
-          start = mid + 1;
-      }
-      return -1;
-    }
-
-  // Binary Search
   int findPairs(vector < int > & nums, int k) {
+        // sort the array so that we can use 2 pointers method
     sort(nums.begin(), nums.end());
-    set<pair<int,int>> pairCount;
-      
-    for (int i = 0; i < nums.size(); i++) {
-      if (binarySearch(nums, i + 1, nums[i] + k)!= -1) {
-        pairCount.insert({ nums[i], nums[i] + k });
-      }
-    }
+    int i = 0, j = 1;
+    set<pair<int, int>> pairCount;
 
+    while (j < nums.size())
+    {
+        int diff = nums[j] - nums[i];
+        if (diff == k)
+        {
+            pairCount.insert({nums[i], nums[j]});
+            i++, j++;
+        }
+        else if (diff > k)
+        {
+            i++; // if diff is getting bigger reduce it
+        }
+        else
+        {
+            j++; // if diff is getting smaller increase it
+        }
+        // edge case 0
+        // any number will be a pair, u can '-' the number from itself and you have a pair
+        if (i == j)
+        {
+            j++;
+        }
+    }
     return pairCount.size();
   }
 };
