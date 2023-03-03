e7f94234aa4c52d8e07d856d3f962970349b869c
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-03-03 23:01:11 +0530

    Time: 4 ms (91.97%), Space: 9.8 MB (96.58%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1;
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return -1;
+    }
+    
+    bool checkIfExist(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        for(int i = 0;i<arr.size();i++){
+            int val = binarySearch(arr, arr[i]*2); 
+            if(val != -1 && val != i){
+                return true;
+            }
+        }
+        return false;
+    }
+};
