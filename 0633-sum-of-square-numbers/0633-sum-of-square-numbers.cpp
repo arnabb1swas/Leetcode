006e726a662131a584210ad8cdb1a57107e51f31
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-03-04 22:59:32 +0530

    Time: 5 ms (41.49%), Space: 5.9 MB (59.75%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool judgeSquareSumTwoPointers(int c){
+    bool judgeSquareSum(int c) {
         long int i = 0, j = (int)sqrt(c);
         if(c<0){
             return false;
@@ -17,28 +17,5 @@ public:
             }
         }
         return false;
-    }
-    
-//     bool judgeSquareSumBinarySearch(int c){
-        
-//     }
-    
-    // int binarySearch(vector<int>& nums, int target) {
-    //     int start = 0, end = nums.size() - 1;
-    //     while (start <= end){
-    //         int mid = start + (end - start) / 2;
-    //         if (nums[mid] == target)
-    //             return mid;
-    //         else if (nums[mid] > target)
-    //             end = mid - 1;
-    //         else
-    //             start = mid + 1;
-    //     }
-    //     return -1;
-    // }
-    
-    bool judgeSquareSum(int c) {
-        return judgeSquareSumTwoPointers(c);
-        // return judgeSquareSumBinarySearch(c);
     }
 };
