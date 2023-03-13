df594bce4f390e84f1158fec3135ac47fc5605d9
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-03-13 10:10:39 +0530

    Time: 13 ms (71.39%), Space: 15.5 MB (80.36%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,34 +1,50 @@
 class Solution {
 public:
-    int binarySearchedindex(vector<int>& arr, int startingIndex,int target){
-    int start=startingIndex;
-        int end = arr.size()-1;
-        int mid = start+(end-start)/2;
+//     int binarySearchedindex(vector<int>& arr, int startingIndex,int target){
+//     int start=startingIndex;
+//         int end = arr.size()-1;
+//         int mid = start+(end-start)/2;
         
-        while(start<=end){
-            if(arr[mid] == target){
-                return mid;
-            }
-            else if(arr[mid]<target){
-                start = mid+1;
-            }else{
-                end = mid-1;
-            }
-            mid = start+(end-start)/2;
-        }
-        return -1;
-    }
+//         while(start<=end){
+//             if(arr[mid] == target){
+//                 return mid;
+//             }
+//             else if(arr[mid]<target){
+//                 start = mid+1;
+//             }else{
+//                 end = mid-1;
+//             }
+//             mid = start+(end-start)/2;
+//         }
+//         return -1;
+//     }
 
     vector<int> twoSum(vector<int>& numbers, int target) {
-        for(int i=0;i<numbers.size();i++){
-            int newTarget = target-numbers[i];
-            int startingIndex = i+1;
-            int foundIndex = binarySearchedindex(numbers,startingIndex,newTarget);
+//         for(int i=0;i<numbers.size();i++){
+//             int newTarget = target-numbers[i];
+//             int startingIndex = i+1;
+//             int foundIndex = binarySearchedindex(numbers,startingIndex,newTarget);
             
-            if(foundIndex != -1){
-                return {startingIndex,foundIndex+1};
+//             if(foundIndex != -1){
+//                 return {startingIndex,foundIndex+1};
+//             }
+//         }
+//         return {};
+        int start = 0 ; 
+        int end = numbers.size() - 1 ; 
+
+        while ((start <= end) && (numbers[start] + numbers[end] != target))
+        {
+            if (numbers[start] + numbers[end] < target)
+            {
+                start ++ ;  
+            }
+            else 
+            {
+                end -- ; 
             }
         }
-        return {};
+        return {start + 1 ,end + 1} ; 
+    
     }
 };
