7b130e96865a02f2ee8ef82d3b51cf0b3e1f9507
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-03-19 12:21:08 +0530

    Time: 46 ms (92.61%), Space: 24.1 MB (45.60%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -10,7 +10,7 @@ public:
     }
     bool validPalindrome(string s) {
         int i = 0, j = s.length() - 1;
-        while(i<=j){
+        while(i<=j){ 
             if(s[i]!=s[j]){
                 return checkPalindrom(s,i+1,j) || checkPalindrom(s,i,j-1);
             }else{
