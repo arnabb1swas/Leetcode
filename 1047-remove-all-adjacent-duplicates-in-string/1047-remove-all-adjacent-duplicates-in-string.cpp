6949f555d55c7a035c6de724e047b8baabc8acd9
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-03-19 12:01:28 +0530

    Time: 22 ms (51.40%), Space: 11 MB (70.95%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -3,12 +3,8 @@ class Solution {
     string a = "";
     int i = 0;
     while (i < s.length()) {
-      if (a.length() > 0) {
-        if (a[a.length() - 1] == s[i]) {
-          a.pop_back();
-        } else {
-          a.push_back(s[i]);
-        }
+      if (a.length() > 0 && a[a.length() - 1] == s[i]) {
+        a.pop_back();
       } else {
         a.push_back(s[i]);
       }
