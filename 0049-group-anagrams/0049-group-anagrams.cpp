2cdcb5d41f4a6b9766654385a69e0d59d2400b77
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-03-24 20:34:25 +0530

    Time: 39 ms (63.46%), Space: 21.6 MB (25.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
+    std::array<int,26> hash(string s){
+        std:array<int, 26>hash = {0};
+        for(int i=0;i<s.size();i++){
+            hash[s[i]-'a']++;
+        }
+        return hash;
+    }
+    // solving by sorting the words.
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//       map <string, vector<string> >mp;
+//       for (auto str: strs) {
+//         string s = str;
+//         sort(s.begin(), s.end());
+//         mp[s].push_back(str);
+//       }
+
+//       vector < vector < string > > ans;
+//       for (auto it = mp.begin(); it != mp.end(); it++) {
+//         ans.push_back(it -> second);
+//       }
+//       return ans;
+//     }
+    // solving by using 26 size lowwercase letter array hashmap
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-      map <string, vector<string> >mp;
+      map <std::array<int, 26>, vector<string> >mp;
+        
       for (auto str: strs) {
-        string s = str;
-        sort(s.begin(), s.end());
-        mp[s].push_back(str);
+        mp[hash(str)].push_back(str);
       }
 
       vector < vector < string > > ans;
