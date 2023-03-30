7ae1094c837832fbc229e2522020746fe2da1a36
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-03-30 21:57:29 +0530

    Time: 3 ms (83.22%), Space: 6.6 MB (98.96%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
-    string addStrings(string num1, string num2) {
-        int len1=num1.length()-1;
-        int len2=num2.length()-1;
-        string ans="";
-        int carry=0;
-        while(len1>=0 || len2>=0 || carry){
+//     string addStrings(string num1, string num2) {
+//         int len1=num1.length()-1;
+//         int len2=num2.length()-1;
+//         string ans="";
+//         int carry=0;
+//         while(len1>=0 || len2>=0 || carry){
             
-            int n1=len1>=0 ?num1[len1]-'0':0;
-            int n2=len2>=0 ?num2[len2]-'0':0;
-            int result=n1+n2+carry;
-            int temp=result%10;
-            carry=result/10;
-            ans+=temp+'0';
-            len1--;
-            len2--;
-        }
+//             int n1=len1>=0 ?num1[len1]-'0':0;
+//             int n2=len2>=0 ?num2[len2]-'0':0;
+//             int result=n1+n2+carry;
+//             int temp=result%10;
+//             carry=result/10;
+//             ans+=temp+'0';
+//             len1--;
+//             len2--;
+//         }
         
-        reverse(ans.begin(),ans.end());
+//         reverse(ans.begin(),ans.end());
+//         return ans;
+//     }
+    
+        
+    // using recursion
+    void addStringUsingRecursion(string& num1, int p1, string& num2, int p2, int carry, string& ans){
+        if(p1<0 && p2<0){
+            if(carry !=0 ){
+                ans.push_back( carry + '0' );
+            }
+            return;
+        }
+    
+        int n1 = (p1 >= 0 ? num1[p1] : '0') - '0';
+        int n2 = (p2 >= 0 ? num2[p2] : '0') - '0';
+        int sum = n1 + n2 + carry;
+        ans.push_back( sum % 10 + '0' );
+        
+        addStringUsingRecursion(num1, --p1, num2, --p2, sum/10, ans);
+    }
+    
+    string addStrings(string num1, string num2) {
+        string ans = "";
+        addStringUsingRecursion(num1, num1.size()-1, num2, num2.size()-1, 0, ans);
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
