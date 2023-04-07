0f9c033ca776cfaf9141d32d14256946e9932dc4
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-04-07 23:30:55 +0530

    Time: 3 ms (65.63%), Space: 5.9 MB (92.98%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,13 +1,18 @@
 class Solution {
   public: int mySqrt(int x) {
     long long s = 0, e = x, ans;
+      
     while (s <= e) {
         long long mid = s + (e-s) / 2;
-        if (mid * mid == x) return mid;
+        if (mid * mid == x) {
+            return mid;
+        }
+        
         else if (mid * mid < x) {
             s = mid + 1;
             ans = mid;
-        } 
+        }
+        
         else {
             e = mid - 1;
         } 
