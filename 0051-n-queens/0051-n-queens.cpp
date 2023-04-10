69a06ebf5b93291960edd243f573d1a07ed0bde9
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-04-10 21:15:32 +0530

    Time: 4 ms (80.77%), Space: 8 MB (38.29%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+// void print(vector<vector<char>> &board,int n)
+// {
+//     for(int i=0;i<n;i++)
+//     {
+//         for(int j=0;j<n;j++)
+//         {
+//             cout<<board[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     cout<<endl;
+// }
+bool issafe(vector<vector<char>> &board,int row,int col,int n)
+{
+    int i=row;
+    int j=col;
+    //FOR ROWS
+    while(j>=0)
+    {
+        if(board[i][j]=='Q')
+        {
+            return false;
+        }
+        j--;
+    }
+    i=row;
+    j=col;
+    //FOR UPPER LEFT
+    while(i>=0 && j>=0)
+    {
+        if(board[i][j]=='Q')
+        {
+            return false;
+        }
+        i--;
+        j--;
+    }
+    i=row;
+    j=col;
+    //FOR BOTTOM LEFT
+    while(i<n && j>=0)
+    {
+        if(board[i][j]=='Q')
+        {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+void solve(vector<vector<char>> &board,int col,int n,vector<vector<string>> &ans)
+{
+    if(col>=n)
+    {
+        vector<string> solution;
+        for(int i=0;i<n;i++)
+        {
+        solution.push_back(string(board[i].begin(),board[i].end()));
+        }
+        ans.push_back(solution);
+        return;
+    }
+    for(int row=0;row<n;row++)
+    {
+        if(issafe(board,row,col,n))
+        {
+            // SOLVING 1 CASE
+            board[row][col]='Q';
+            // RECURSIVE CALL
+            solve(board,col+1,n,ans);
+            // BACKTRACKING
+            board[row][col]='.';
+        }
+    }
+}
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<char>> board(n,vector<char>(n,'.'));
+        int col=0;
+        vector<vector<string>> ans;
+        solve(board,col,n,ans);
+        return ans;
+
+    }
+};
+
+
