aa7a7e41003c31c97d7d1cafb3506186d6c76614
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-04-19 21:52:45 +0530

    Time: 12 ms (35.81%), Space: 7.7 MB (42.03%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -39,6 +39,7 @@ class Solution {
     void solve(vector<vector<char>>& board, int col, int n, vector<vector<string>>& ans) {
         if (col >= n) {
             vector < string > solution;
+            
             for (int i = 0; i < n; i++) {
                 solution.push_back(string(board[i].begin(), board[i].end()));
             }
@@ -61,6 +62,7 @@ class Solution {
     vector<vector<string>>solveNQueens(int n) {
         vector<vector<char>>board(n, vector<char> (n, '.'));
         int col = 0;
+        
         vector<vector<string>> ans;
         solve(board, col, n, ans);
         
