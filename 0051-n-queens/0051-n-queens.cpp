994b773f28bb58c9dac7783c339d2c412ec3fcbb
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-05-06 12:02:35 +0530

    Time: 15 ms (29.61%), Space: 8.2 MB (29.72%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,72 +1,75 @@
 class Solution {
-  public:
-    bool issafe(vector<vector<char>>& board, int row, int col, int n){
-        int i = row;
-        int j = col;
-        
-        //FOR ROWS
-        while (j >= 0) {
-            if (board[i][j] == 'Q') {
-                return false;
+public:
+
+    unordered_map<int,bool> rowCheck;
+    unordered_map<int,bool> upperLeftDiagnolCheck;
+    unordered_map<int,bool> bottomLeftDiagnolCheck;
+
+    void storeSolution(vector<vector<char>> &board, int n, vector<vector<string> > &ans) {
+        vector<string> temp;
+        for(int i=0; i<n; i++) {
+            string output = "";
+            for(int j=0; j<n ;j++) {
+                output.push_back(board[i][j]);
             }
-            j--;
+            temp.push_back(output);
         }
-        i = row;
-        j = col;
-        
-        //FOR UPPER LEFT
-        while (i >= 0 && j >= 0){
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-            i--;
-            j--;
-        }
-        i = row;
-        j = col;
-        
-        //FOR BOTTOM LEFT
-        while (i < n && j >= 0){
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-            i++; j--;
-        }
-        
-        return true;
+        ans.push_back(temp);
     }
-    
-    void solve(vector<vector<char>>& board, int col, int n, vector<vector<string>>& ans) {
-        if (col >= n) {
-            vector < string > solution;
-            
-            for (int i = 0; i < n; i++) {
-                solution.push_back(string(board[i].begin(), board[i].end()));
-            }
-            ans.push_back(solution);
-            
-            return;
+
+
+    bool isSafe(int row, int col, vector<vector<char>> &board, int n) {
+
+        if(rowCheck[row] == true ){
+             return false;
         }
-        
-        for (int row = 0; row < n; row++) {
-            if (issafe(board, row, col, n)) {
-                // SOLVING 1 CASE
-                board[row][col] = 'Q';
-                // RECURSIVE CALL
-                solve(board, col + 1, n, ans);
-                // BACKTRACKING
-                board[row][col] = '.';
-            }
+           
+        if(upperLeftDiagnolCheck[n-1+col-row] == true){
+             return false;
         }
+            
+        if(bottomLeftDiagnolCheck[row+col] == true){
+             return false;
+        }
+
+        return true; 
+}
+
+
+    void solve(vector<vector<char>> &board, int col, int n, vector<vector<string> > &ans  ) {
+    //base case
+    if(col >= n) {
+        storeSolution(board, n, ans);
+        return ;
     }
-    
-    vector<vector<string>>solveNQueens(int n) {
-        vector<vector<char>> board(n, vector<char> (n, '.'));
+
+    //1 case solve karna h , baaki recursion sambhal lega
+    for(int row = 0; row <n; row++) {
+        if(isSafe(row, col, board, n)) {
+            board[row][col] = 'Q';
+            rowCheck[row] = true;
+            upperLeftDiagnolCheck[n-1+col-row] = true;
+            bottomLeftDiagnolCheck[row+col] = true;
+
+            //recursion solution laega
+            solve(board, col+1, n, ans);
+            
+            //backtracking
+            board[row][col] = '.';
+            rowCheck[row] = false;
+            upperLeftDiagnolCheck[n-1+col-row] = false;
+            bottomLeftDiagnolCheck[row+col] = false;
+        }
+
+    }
+}
+
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<char>> board(n, vector<char>(n,'.'));
+        vector<vector<string> > ans;
         int col = 0;
-        
-        vector<vector<string>> ans;
         solve(board, col, n, ans);
-        
         return ans;
     }
 };
