c56ea75f90648b34721c6411174c77118b92bdf1
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-05-13 14:45:24 +0530

    Time: 0 ms (100.00%), Space: 15.7 MB (26.07%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void solve(vector<string>& ans, string output, int open, int close){
+        // base case: we have used all the opening and closing brackets that we have apossible solution
+        if(open == 0 && close == 0){
+            ans.push_back(output);
+            return;
+        }
+        
+        // include opening bracket
+        if(open > 0){
+            output.push_back('(');
+            solve(ans, output, open-1, close);
+            output.pop_back();
+        }
+
+        // include closing bracket
+        if(close > open){
+            output.push_back(')');
+            solve(ans, output, open, close-1);
+            output.pop_back();
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string output = "";
+        int open = n;
+        int close = n;
+        
+        solve(ans,output,open,close);
+        return ans;
+    }
+};
