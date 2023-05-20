188ba9c6e68316f7f7e7f3ab0f31f8dc7bf36242
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-05-20 19:44:45 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (99.42%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -72,8 +72,8 @@ public:
 			if((cur&r) || (cur&d1) || (cur&d2)) continue;
 			dfs(n,dep+1, (cur|r),(cur|d1)<<1,(cur|d2)>>1);
 		}
-
 	}
+    
 	int totalNQueens(int n) {
 		res=0;
 		dfs(n,0,0,0,0);
