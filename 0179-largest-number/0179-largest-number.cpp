e55611124b3191ea7296487435e071ca97490e44
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-05-22 22:42:28 +0530

    Time: 9 ms (58.37%), Space: 11.2 MB (78.27%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,22 +1,47 @@
 class Solution {
 public:
-    static bool comparator(string a, string b){
-        return a+b>b+a;
+    static bool check(string &s1, string &s2){
+        return (s1 +s2)>=(s2 + s1);
     }
     
-    string largestNumber(vector<int>& nums) {
-        vector<string> ans;
-        for(int i:nums){
-            ans.push_back(to_string(i));
-        }
+//     string largestNumber(vector<int>& nums) {
+//         vector<string> ans;
+//         for(int i:nums){
+//             ans.push_back(to_string(i));
+//         }
         
-        sort(ans.begin(), ans.end(), comparator);
+//         sort(ans.begin(), ans.end(), comparator);
 
-        string result;
-        for(int i=0; i<ans.size(); i++){
-            result+=ans[i];
+//         string result;
+//         for(int i=0; i<ans.size(); i++){
+//             result+=ans[i];
+//         }
+
+//         return result[0]=='0'? "0" : result;
+//     }
+
+    string largestNumber(vector<int>& nums) {
+        int x = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 0){
+                x++;
+            }
+        }
+        if(x == nums.size()) return "0";
+
+        vector<string> s(nums.size(), "");
+
+        for(int i=0; i<nums.size();i++){
+            s[i]= to_string(nums[i]);
         }
 
-        return result[0]=='0'? "0" : result;
+        sort(s.begin(), s.end(), check);
+        string ans = "";
+
+        for(int i=0; i<s.size(); i++){
+            ans += (s[i]);
+        }
+
+        return ans;
     }
 };
