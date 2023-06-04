52a228da6c37c3a65dbadb3ceebf82b354f94481
Arnab Biswas <44947372+arnabb1swas@users.noreply.github.com>
2023-06-04 23:37:11 +0530

    Time: 3 ms (93.06%), Space: 10.7 MB (71.98%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
-    void solve(int target, vector<int>& candidates, int idx, vector<vector<int>>& v, vector<int> &ans){
-        if(idx == candidates.size()){
-            if(target == 0){
-                v.push_back(ans);
-            }
+//     void solve(int target, vector<int>& candidates, int idx, vector<vector<int>>& v, vector<int> &ans){
+//         if(idx == candidates.size()){
+//             if(target == 0){
+//                 v.push_back(ans);
+//             }
             
-            return; 
-        }
-        if(target>=candidates[idx]){
-            ans.push_back(candidates[idx]);
-            solve(target-candidates[idx],candidates,idx,v,ans);
-            ans.pop_back();
-        }
+//             return; 
+//         }
+//         if(target>=candidates[idx]){
+//             ans.push_back(candidates[idx]);
+//             solve(target-candidates[idx],candidates,idx,v,ans);
+//             ans.pop_back();
+//         }
         
-        solve(target,candidates,idx+1,v,ans);
+//         solve(target,candidates,idx+1,v,ans);
+//     }
+    
+    void find_subs(int i,vector<int> &candidates,vector<vector<int>> &ans, int target, vector<int> &list)
+    {
+      if(i>=candidates.size())
+      {   
+          if(target==0)
+             ans.push_back(list);
+          return;
+      }
+      if(candidates[i]<=target)
+      {
+      list.push_back(candidates[i]);
+      find_subs(i,candidates,ans,target-candidates[i],list);
+      list.pop_back();
+      }
+      find_subs(i+1,candidates,ans,target,list);
+    }
+    
+        vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+       vector<vector<int>> ans;
+       vector<int> list;
+       find_subs(0,candidates,ans,target,list);
+       return ans; 
     }
 
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector <int> ans;
-        vector <vector<int>> v;
-        solve(target,candidates,0,v,ans);
-        return v;
-    }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector <int> ans;
+//         vector <vector<int>> v;
+//         solve(target,candidates,0,v,ans);
+//         return v;
+//     }
 };
